f1a03bf2293922a9474e922dfd2da87c3dc4a8d7
Jose Julián <Jose Julin@DESKTOP-LRPLOP7>
2024-06-29 15:51:10 -0600

    Carga inicial

---
+++ src/main/java/com/mycompany/proyectojava/PROYECTOJAVA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectojava;
+
+/**
+ *
+ * @author Jose Julián
+ */
+public class PROYECTOJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
